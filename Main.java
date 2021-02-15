04b7a1aa89ea348c77dfc25c793d35b1a2af461d
Fatima Avila <fatimaavila@ufm.edu>
2021-02-15 22:14:59 +0000

    últimos cambios de programa

--- Main.java
+++ Main.java
@@ -7,7 +7,9 @@ import java.util.Collections;
 
 
 class Main {
-static Map<Character,Integer> map1 = new HashMap<Character,Integer>(); //array asociativo para key y value         
+static Map<Character,Integer> map1 = new HashMap<Character,Integer>(); //array asociativo para key y value   
+
+ static ArrayList<Integer> list = new ArrayList<>(); // lista para guardar scores y poder ordenar top10
 
 private static String convertToStars(int num) {
     StringBuilder builder = new StringBuilder();
@@ -32,7 +34,35 @@ public static void cuentaLetras(char[] c)
       }
     } //todo el parrafo
 
-}
+} //cuentaLetras
+
+public static void topScores(){
+   for (Map.Entry<Character, Integer> entry : map1.entrySet())
+    {
+         list.add(entry.getValue()); // guarda los scores en una lista para ordenarlos
+    } // termina for 
+
+     // Ordenamiento de los puestos mayor a menor
+	   Collections.sort(list, Collections.reverseOrder());
+} //top topScores
+
+public static void despliegaHistograma() {
+  ArrayList<Character> mostrados = new ArrayList<>();
+     for(int k=0; k < 10; k++) {
+       for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
+      if (entry.getValue() == list.get(k) && mostrados.contains(entry.getKey())== false){
+        mostrados.add(entry.getKey());
+        System.out.println("Char " + entry.getKey() + ":" + entry.getValue() + " "+ "\u001B[36m"+convertToStars(entry.getValue())+"\u001B[0m");
+        
+        } //si el valor es el mismo
+      
+    } // termina for del map1
+
+    } // fin for2
+
+} // despliegaHistograma
+
+
 ///======================== main=====================
 
   public static void main(String[] args) {
@@ -48,38 +78,11 @@ public static void cuentaLetras(char[] c)
         char[] c=parrafo.toCharArray();
         
         cuentaLetras(c);  //llena array asociativo
-   
-
-    //Now To find the highest character repeated 
-    //int max=0;
-    //char maxCharacter = 'a';//setting to a by default
-    System.out.println("\n");
-    ArrayList<Integer> list = new ArrayList<>();
-    for (Map.Entry<Character, Integer> entry : map1.entrySet())
-    {
-        //System.out.println("Key= " + entry.getKey() + " " + entry.getValue() + " "+ convertToStars(entry.getValue()));
-         list.add(entry.getValue());
-      
-    } // termina for 
-
-     /* Sorting in decreasing order*/
-	   Collections.sort(list, Collections.reverseOrder());
+        topScores(); // ordenados todos los scores
+        System.out.println("\n"+"\u001B[36m"+"----Histograma caracteres más utilizados-----"+"\u001B[0m");
+        System.out.println("\n");
+        despliegaHistograma();  
      
-     ArrayList<Character> mostrados = new ArrayList<>();
-
-     for(int k=0; k < 10; k++) {
-       for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
-      if (entry.getValue() == list.get(k) && mostrados.contains(entry.getKey())== false){
-        mostrados.add(entry.getKey());
-        System.out.println("Key=" + entry.getKey() + " " + entry.getValue() + " "+ "\u001B[36m"+convertToStars(entry.getValue())+"\u001B[0m");
-        
-        } //si el valor es el mismo
-      
-    } // termina for del map1
-
-    } // fin for2
-
-  
   } //fin metodo main
 
   } //fin main class
